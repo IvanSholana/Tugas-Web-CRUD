f2b8beea6f08ba715970730ad7d9b1d96369207f
Ivan Sholana <94579033+IvanSholana@users.noreply.github.com>
2023-05-24 14:46:08 +0700

    tinggal ngumpulin

--- src/ShowData.php
+++ src/ShowData.php
@@ -13,7 +13,7 @@ $connection = koneksi("mahasiswa");
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Tampil Data</title>
     <link rel="stylesheet" href="../dist/output.css" />
   </head>
   <body class="bg-slate-300">
